5d41d4a57d76208318b21397eeddb4932abe9706
Sachin Kumar Chaudhary <sachin.k.chaudhary.in@gmail.com>
2026-09-04 20:58:58 +0530

    style: finish lint cleanup

--- src/sentinellayer_growth_engine/mail_handoff.py
+++ src/sentinellayer_growth_engine/mail_handoff.py
@@ -5,7 +5,12 @@ from datetime import UTC, datetime
 from typing import Any
 from uuid import UUID, uuid4
 
-from .contracts import ContractValidationError, validate_rendered_send_treatment, validate_send_request
+from .contracts import (
+    ContractValidationError,
+    validate_rendered_send_treatment,
+    validate_send_request,
+)
+
 
 class CampaignMailHandoffError(ValueError):
     """Raised when Campaign cannot safely construct a Mail request."""
