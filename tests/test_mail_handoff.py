44d5d20619a3e929bdaa707215544ebca17562b1
Sachin Kumar Chaudhary <sachin.k.chaudhary.in@gmail.com>
2026-09-04 21:25:43 +0530

    test: use timezone-aware fixture datetimes

--- tests/test_mail_handoff.py
+++ tests/test_mail_handoff.py
@@ -49,7 +49,7 @@ def test_rejects_invalid_treatment():
         CampaignMailHandoff().build_send_request(
             treatment=bad,
             mailbox_id=U["mailbox"],
-            scheduled_at=datetime(2026, 9, 2, 12),
+            scheduled_at=datetime(2026, 9, 2, 12, tzinfo=UTC),
         )
 
 def test_rejects_naive_schedule():
