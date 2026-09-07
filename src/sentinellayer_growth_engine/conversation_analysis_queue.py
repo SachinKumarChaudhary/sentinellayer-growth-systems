e9585b041d1f3ce953fecb65d0244915965191fe
Sachin Kumar Chaudhary <sachin.k.chaudhary.in@gmail.com>
2026-09-07 20:02:17 +0530

    Correct psycopg escaping for PostgreSQL conversation format placeholders

--- src/sentinellayer_growth_engine/conversation_analysis_queue.py
+++ src/sentinellayer_growth_engine/conversation_analysis_queue.py
@@ -125,7 +125,7 @@ class ConversationAnalysisQueue:
                         select coalesce(
                             string_agg(
                                 format(
-                                    '[%%s] %s\\nSubject: %s\\n%s',
+                                    '[%%s] %%s\\nSubject: %%s\\n%%s',
                                     direction,
                                     occurred_at,
                                     subject,
@@ -146,14 +146,7 @@ class ConversationAnalysisQueue:
                     cross join history h
                     where j.analysis_id=%s
                     """,
-                    (
-                        batch_size,
-                        self._worker_id,
-                        lease_seconds,
-                        row["reply_id"],
-                        row["reply_id"],
-                        row["analysis_id"],
-                    ),
+                    (row["reply_id"], row["reply_id"], row["analysis_id"]),
                 )
                 detail = cur.fetchone()
                 if detail is None:
