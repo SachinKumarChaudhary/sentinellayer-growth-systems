486c02078827b8313fb5dc0f41ce6e15a056a43c
Sachin Kumar Chaudhary <sachin.k.chaudhary.in@gmail.com>
2026-09-09 10:49:10 +0530

    chore: remove redundant refresh contract test

--- tests/test_tinyfish_refresh_extra.py
+++
@@ -1,6 +0,0 @@
-def test_refresh_result_contract_is_stable():
-    from sentinellayer_growth_engine.tinyfish_batch import TinyFishRefreshRunResult
-
-    result = TinyFishRefreshRunResult(1, 1, 0, [1], [], 7)
-    assert result.requested == 1
-    assert result.min_age_days == 7
