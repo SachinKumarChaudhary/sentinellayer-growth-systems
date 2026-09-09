70a9869668ce323d7f661087684687dc67e732f0
Sachin Kumar Chaudhary <sachin.k.chaudhary.in@gmail.com>
2026-09-09 10:48:27 +0530

    chore: remove redundant protocol test

--- tests/test_tinyfish_batch_protocol.py
+++
@@ -1,7 +0,0 @@
-from sentinellayer_growth_engine.tinyfish_batch import EnrichmentRepositoryPort
-
-
-def test_repository_protocol_exposes_refresh_queue() -> None:
-    assert "next_refresh_company_ids" in getattr(EnrichmentRepositoryPort, "__annotations__", {}) or hasattr(
-        EnrichmentRepositoryPort, "next_refresh_company_ids"
-    )
